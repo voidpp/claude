bf17f18f3fb3aff0fae6811e7ebc7aba43dd2b7d
Lajos Santa <santa.lajos@gmail.com>
2023-02-11 21:56:26 +0100

    enh: select values in sunrise widget

--- frontend/components/widgets/sunrise-sunset.tsx
+++ frontend/components/widgets/sunrise-sunset.tsx
@@ -1,32 +1,89 @@
 import { SunriseSunset as SunriseSunsetType, useSunriseSunsetQuery } from "@/graphql-types-and-hooks";
+import { useInterval } from "@/hooks";
 import { BaseWidgetSettings, CommonWidgetProps } from "@/types";
 import { Box } from "@mui/system";
 import dayjs from "dayjs";
 import * as React from "react";
-import { useInterval } from "../../hooks";
 import { RndFrame, useRnd } from "../rnd";
 import { WidgetMenu } from "../widget-menu";
+import { FormCheckboxListFieldDescriptor } from "../widget-settings-dialog";
+
+type SunriseSunsetKeys = keyof Omit<SunriseSunsetType, "__typename">;
+
+type ValueDescriptor = {
+    label: string;
+    showDefault: boolean;
+    formatter: (value: any) => string;
+};
+
+const timeFormmatter = (value: string) => dayjs(value).format("HH:mm");
+
+const valuesDescriptors: Record<SunriseSunsetKeys, ValueDescriptor> = {
+    sunrise: {
+        label: "Sunrise",
+        showDefault: true,
+        formatter: timeFormmatter,
+    },
+    sunset: {
+        label: "Sunset",
+        showDefault: true,
+        formatter: timeFormmatter,
+    },
+    solarNoon: {
+        label: "Solar noon",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    dayLength: {
+        label: "Day length",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    civilTwilightBegin: {
+        label: "Civil twilight begin",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    civilTwilightEnd: {
+        label: "Civil twilight end",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    nauticalTwilightBegin: {
+        label: "Nautical twilight begin",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    nauticalTwilightEnd: {
+        label: "Nautical twilight end",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    astronomicalTwilightBegin: {
+        label: "Astronomical twilight begin",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+    astronomicalTwilightEnd: {
+        label: "Astronomical twilight end",
+        showDefault: false,
+        formatter: timeFormmatter,
+    },
+};
+
+type Layout = "vertical" | "horizontal";
 
 export class SunriseSunsetSettings extends BaseWidgetSettings {
     city: string = "Budapest";
+    values: Record<string, boolean> = Object.fromEntries(
+        Object.entries(valuesDescriptors).map(([key, desc]) => [key, desc.showDefault])
+    );
+    fontSize: string = "1.1em";
 }
 
 const dayInMsecs = 1000 * 60 * 60 * 24;
 
 export type SunriseSunsetProps = CommonWidgetProps<SunriseSunsetSettings>;
-
-const labels: Record<keyof Omit<SunriseSunsetType, "__typename">, string> = {
-    sunrise: "Sunrise",
-    sunset: "Snset",
-    solarNoon: "Solar noon",
-    dayLength: "Day length",
-    civilTwilightBegin: "Civil twilight begin",
-    civilTwilightEnd: "Civil twilight end",
-    nauticalTwilightBegin: "Nautical twilight begin",
-    nauticalTwilightEnd: "Nautical twilight end",
-    astronomicalTwilightBegin: "Astronomical twilight begin",
-    astronomicalTwilightEnd: "Astronomical twilight end",
-};
 
 export const SunriseSunset = (props: SunriseSunsetProps) => {
     const { config } = props;
@@ -41,16 +98,18 @@ export const SunriseSunset = (props: SunriseSunsetProps) => {
     return (
         <RndFrame rndProps={rndProps}>
             <Box sx={{ p: 1 }}>
-                <Box component="table" sx={{ fontSize: "1.1em", "& td": { p: 1 } }}>
+                <Box component="table" sx={{ fontSize: config.settings.fontSize, "& td": { p: 1 } }}>
                     <tbody>
-                        <tr>
-                            <td>Sunrise:</td>
-                            <td>{dayjs(data?.sunriseSunset.sunrise).format("HH:mm")}</td>
-                        </tr>
-                        <tr>
-                            <td>Sunset:</td>
-                            <td>{dayjs(data?.sunriseSunset.sunset).format("HH:mm")}</td>
-                        </tr>
+                        {Object.entries(config.settings.values).map(([key, isShow]) => {
+                            if (!isShow) return null;
+                            const propName = key as SunriseSunsetKeys;
+                            return (
+                                <tr key={propName}>
+                                    <td>{valuesDescriptors[propName].label}:</td>
+                                    <td>{valuesDescriptors[propName].formatter(data?.sunriseSunset[propName])}</td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </Box>
             </Box>
@@ -63,6 +122,16 @@ export const SunriseSunset = (props: SunriseSunsetProps) => {
                         name: "city",
                         label: "City",
                     },
+                    {
+                        name: "fontSize",
+                        label: "Font size",
+                    },
+                    {
+                        type: "checkboxList",
+                        name: "values",
+                        label: "Values to show",
+                        options: Object.entries(valuesDescriptors).map(([key, { label }]) => ({ value: key, label })),
+                    } as FormCheckboxListFieldDescriptor,
                 ]}
             />
         </RndFrame>
